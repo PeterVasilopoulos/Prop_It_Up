4171a191af0f865e12b9824a3cacd6a69bb4cebf
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-05-04 15:15:14 -0400

    Assignment completed

--- prop_it_up/src/App.js
+++ prop_it_up/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
+import PersonCard from "./components/PersonCard";
 
 function App() {
   return (
       <div>
-        <h1>Hello Dojo!</h1>
-        <h2>Things I need to do:</h2>
-        <ul>
-          <li>Learn React</li>
-          <li>Climb Mt. Everst</li>
-          <li>Run a marathon</li>
-          <li>Feed the dogs</li>
-        </ul>
+        <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {'Black'}/>
+        
+        <PersonCard firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {'Brown'}/>
+
+        <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {'Brown'}/>
+
+        <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {'Brown'}/>
       </div>
   );
 }
